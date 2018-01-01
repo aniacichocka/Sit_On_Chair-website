183b58cb72e83cc2caefb25879e5f1d1ff9968b6
aniacichocka <aniacichocka86@gmail.com>
2018-01-01 15:28:42 +0100

    Menu, bars, slider - done

---
+++ script.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded',function () {
+// console.log('działa');
+
+  function removeBar() {
+    var pictures = document.querySelectorAll('.images');
+
+    pictures.forEach(function(element) {
+      element.addEventListener('mouseover', function() {
+
+        var pictureBars = this.querySelectorAll('.opacity-area');
+
+        pictureBars.forEach(function(bar) {
+          bar.style.display ='none';
+        })
+      })
+
+      element.addEventListener('mouseout', function() {
+
+        var pictureBars = this.querySelectorAll('.opacity-area');
+
+        pictureBars.forEach(function(bar) {
+          bar.style.display ='table-cell';
+        })
+      })
+    })
+  }
+  removeBar();
+
+  function slider() {
+    var body = document.querySelector('body');
+
+    var prev = body.querySelector('.lt-sign');
+    var next = body.querySelector('.gt-sign');
+
+    var listImg = body.querySelectorAll('.main-slide');
+    console.log(listImg);
+
+    var index = 0;
+
+    listImg[index].classList.add('visible');
+
+    next.addEventListener('click', function() {
+
+      listImg[index].classList.remove('visible');
+      index += 1;
+
+      if (index > listImg.length-1) {
+        index = 0;
+      }
+
+      listImg[index].classList.add('visible');
+    })
+
+    prev.addEventListener('click', function() {
+
+      listImg[index].classList.remove('visible');
+      index -= 1;
+
+      if (index < 0) {
+        index = listImg.length-1;
+      }
+
+      listImg[index].classList.add('visible');
+    })
+  }
+  slider();
+
+})
