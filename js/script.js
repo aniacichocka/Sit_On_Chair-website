b6c1b061a82dbf15d11d9378decbff0ddc21ec9f
aniacichocka <aniacichocka86@gmail.com>
2018-01-20 01:18:59 +0100

    Calculator - done

--- js/script.js
+++ js/script.js
@@ -79,4 +79,126 @@ document.addEventListener('DOMContentLoaded',function () {
   }
   dropDown();
 
+  function order() {
+
+    var body = document.querySelector('body');
+
+    var productList = body.querySelector('.sum-area-left');
+    var nameProduct =  body.querySelector('.sum-area-left span');
+    var option1 = body.querySelector('.sum-area-left p:first-of-type');
+    var option2 = body.querySelector('.sum-area-left p:nth-of-type(2)');
+    var option3 = body.querySelector('.sum-area-left p:last-of-type');
+    var priceList = body.querySelector('.sum-area-right');
+    var typeListLi = body.querySelectorAll('.type-list li');
+    var colorListLi = body.querySelectorAll('.color-list li');
+    var materialListLi = body.querySelectorAll('.material-list li');
+    var transportPrice = body.querySelector('label');
+    var sum = body.querySelector('.sum-area-bottom p');
+
+
+    function addElement(price, idElement) {
+      var newP = document.createElement('p');
+      newP.innerText = price;
+      newP.id = idElement;
+      priceList.appendChild(newP);
+    }
+
+
+    function addPrice(){
+        var array = document.querySelectorAll('.sum-area-right p');
+        var amount = 0;
+
+        for (var i = 0; i < array.length; i++) {
+          amount += parseInt(array[i].innerText);
+        }
+        sum.innerText = amount + " zl";
+    }
+
+
+    typeListLi.forEach(function(element) {
+      element.addEventListener('click', function() {
+
+        var name = this.innerText;
+        var price = this.dataset.price;
+
+        var idChair = 'chair-price';
+        var idElementPrice = document.getElementById('chair-price');
+
+        if (idElementPrice) {
+          nameProduct.innerText = name;
+          idElementPrice.parentElement.removeChild(idElementPrice);
+          addElement(price, idChair);
+        } else {
+          nameProduct.innerText = name;
+          addElement(price, idChair);
+        }
+        addPrice();
+      })
+    })
+
+
+    colorListLi.forEach(function(element) {
+      element.addEventListener('click', function() {
+
+        var name = this.innerText;
+        var price = this.dataset.price;
+
+        var idColor = 'color-price';
+        var idElementPrice = document.getElementById('color-price');
+
+        if (idElementPrice) {
+          option1.innerText = name;
+          idElementPrice.parentElement.removeChild(idElementPrice);
+          addElement(price, idColor);
+        } else {
+          option1.innerText = name;
+          addElement(price, idColor);
+        }
+        addPrice();
+      })
+    })
+
+
+    materialListLi.forEach(function(element) {
+      element.addEventListener('click', function() {
+
+        var name = this.innerText;
+        var price = this.dataset.price;
+
+        var idMaterial = 'material-price';
+        var idElementPrice = document.getElementById('material-price');
+
+        if (idElementPrice) {
+          option2.innerText = name;
+          idElementPrice.parentElement.removeChild(idElementPrice);
+          addElement(price, idMaterial);
+        } else {
+          option2.innerText = name;
+          addElement(price, idMaterial);
+        }
+        addPrice();
+      })
+    })
+
+
+    transportPrice.addEventListener('click', function() {
+
+      var name = this.innerText;
+      var price = this.lastElementChild.dataset.price;
+
+      var idTransport = 'transport-price';
+      var idElementPrice = document.getElementById('transport-price');
+
+      if (idElementPrice) {
+        option3.innerText = '';
+        idElementPrice.parentElement.removeChild(idElementPrice);
+      } else {
+        option3.innerText = name;
+        addElement(price, idTransport);
+      }
+      addPrice();
+    })
+  }
+  order();
+  
 })
